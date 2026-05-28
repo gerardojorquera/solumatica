6225c17757febb41d97e60af1b6c6189356bf7c1
gerardojorquera <gerardo.jorquera@hotmail.com>
2026-05-28 12:51:29 -0400

    Form enviar correo

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ emailjs.init(
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que la página se recargue
 
+    const subject = document.getElementById('subject').value;
     const name = document.getElementById('user_name').value;
     const email = document.getElementById('user_email').value;
     const message = document.getElementById('user_message').value;
@@ -17,22 +18,26 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     // alert(name + " // " + email + " // " + message);
     // 3. Recolectar los datos escritos por el usuario
     const datosFormulario = {
-        title: "Este es el título de mi correo (Solumatica)",
+        title: "Solumática - Formulario Web de contacto: " + subject,
         //from_name: document.getElementById('user_name').value,
         //user_email: document.getElementById('user_email').value,
         name: name,
         reply_to: email,
         message: message
     };
+    // console.log("Datos del formulario:", datosFormulario);
 
     // 4. Enviar el correo usando EmailJS
     // emailjs.send("TU_SERVICE_ID", "TU_TEMPLATE_ID", datosFormulario)
     emailjs.send("service_payxvxb", "template_b56fnaa", datosFormulario)
         .then(function(response) {
-            alert("¡Correo enviado con éxito!");
-            document.getElementById('contact-form').reset(); // Limpia el formulario
+            //alert("¡Correo enviado con éxito!");
+            document.getElementById('success').innerHTML = "<div class='alert alert-success'>¡Correo enviado con éxito!</div>";
+            // Limpia el formulario
+            document.getElementById('contact-form').reset();
         }, function(error) {
-            alert("Hubo un error al enviar el mensaje. Inténtalo de nuevo.");
+            //alert("Hubo un error al enviar el mensaje. Inténtalo de nuevo.");
+            document.getElementById('success').innerHTML = "<div class='alert alert-danger'>Hubo un error al enviar el mensaje. Inténtalo de nuevo.</div>";
             console.error("Detalle del error:", error);
         });
 });
