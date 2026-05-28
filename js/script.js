cf0d552ebe792b55c225b2960fce64adb7423eb3
gerardojorquera <gerardo.jorquera@hotmail.com>
2026-05-28 16:37:45 -0400

    Se agrega una validacion durante el envio del correo para que se llenen todos los campos

--- js/script.js
+++ js/script.js
@@ -11,36 +11,35 @@ emailjs.init(
     event.preventDefault(); // Evita que la página se recargue */
 function onSubmit(token) {
     // alert("¡Google me llamó y el token es: " + token);
-    
+    // 3. Recolectar los datos escritos por el usuario
     const subject = document.getElementById('subject').value;
     const name = document.getElementById('user_name').value;
     const email = document.getElementById('user_email').value;
     const message = document.getElementById('user_message').value;
 
-    // alert(subject + " // " + name + " // " + email + " // " + message);
-    // 3. Recolectar los datos escritos por el usuario
-    const datosFormulario = {
-        title: "Solumática - Formulario Web de contacto: " + subject,
-        //from_name: document.getElementById('user_name').value,
-        //user_email: document.getElementById('user_email').value,
-        name: name,
-        reply_to: email,
-        message: message
-    };
-    // console.log("Datos del formulario:", datosFormulario);
+    if (!name.trim() || !email.trim() || !message.trim()) {
+        document.getElementById('success').innerHTML = "<div class='alert alert-danger'>Debe ingresar todos los campos.</div>";
+    } else {
+        const datosFormulario = {
+            title: "Solumática - Formulario Web de contacto: " + subject,
+            //from_name: document.getElementById('user_name').value,
+            //user_email: document.getElementById('user_email').value,
+            name: name,
+            reply_to: email,
+            message: message
+        };
+        // console.log("Datos del formulario:", datosFormulario);
 
-    // 4. Enviar el correo usando EmailJS
-    // emailjs.send("TU_SERVICE_ID", "TU_TEMPLATE_ID", datosFormulario)
-    emailjs.send("service_payxvxb", "template_b56fnaa", datosFormulario)
-        .then(function(response) {
-            // alert("¡Correo enviado con éxito!");
-            document.getElementById('success').innerHTML = "<div class='alert alert-success'>¡Correo enviado con éxito!</div>";
-            // Limpia el formulario
-            document.getElementById('contact-form').reset();
-        }, function(error) {
-            // alert("Hubo un error al enviar el mensaje. Inténtalo de nuevo.");
-            document.getElementById('success').innerHTML = "<div class='alert alert-danger'>Hubo un error al enviar el mensaje. Inténtalo de nuevo.</div>";
-            console.error("Detalle del error:", error);
-        });
-//});
+        // 4. Enviar el correo usando EmailJS: emailjs.send("TU_SERVICE_ID", "TU_TEMPLATE_ID", datosFormulario)
+        emailjs.send("service_payxvxb", "template_b56fnaa", datosFormulario)
+            .then(function(response) {
+                document.getElementById('success').innerHTML = "<div class='alert alert-success'>¡Correo enviado con éxito!</div>";
+                // Limpia el formulario
+                document.getElementById('contact-form').reset();
+            }, function(error) {
+                document.getElementById('success').innerHTML = "<div class='alert alert-danger'>Hubo un error al enviar el mensaje. Inténtalo de nuevo.</div>";
+                // console.error("Detalle del error: ", error);
+            });
     }
+}
+//});
